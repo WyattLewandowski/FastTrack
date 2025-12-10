b509b626ea3866da52f7b49ffb5706e025f77222
WyattLewandowski <wyattlewandowski@student.hcps.org>
2025-12-10 12:45:04 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
-let newX = 0, let newY = 0, startX = 0, startY = 0;
+// Make the DIV element draggable:
+dragElement(document.getElementById("mydiv"));
 
-const card = document.getElementById('card')
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+    // if present, the header is where you move the DIV from:
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+  } else {
+    // otherwise, move the DIV from anywhere inside the DIV:
+    elmnt.onmousedown = dragMouseDown;
+  }
 
-card.addEventListener('mousedown', mouseDown)
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
 
-function mouseDown(e){
-startX = e.clientX
-startY = e.clientY
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
 
-document.addEventListener('mousemove', mouseMove)
-document.addEventListener('mouseup', mouseUp)
-}
-
-function mouseMove(e){
-newX = startX - e.clientX
-newY = startY - e.clientY
-
-  startX = e.clientX
-  startY = e.clientY
-
-  card.style.top = startY + 'px'
-  card.style.left = startX + 'px'
-  console.log({newX, newY})
-  console.log({startX, startY})
-}
-function mouseUp(e){
-document.removeEventListener('mousemove', mouseMove)
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
 }
